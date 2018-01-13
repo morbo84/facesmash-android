209d47506b291c8b5c2029f4d11c58969ef8dfd1
Paolo Monteverde <paolo.monteverde@gmail.com>
2018-01-13 16:23:40 +0100

    fixed immersive mode

--- app/src/main/java/com/cynny/gamee/facesmash/FaceSmashActivity.java
+++ app/src/main/java/com/cynny/gamee/facesmash/FaceSmashActivity.java
@@ -58,11 +58,6 @@ public class FaceSmashActivity extends SDLActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 19) {
-            int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
-            uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
-            getWindow().getDecorView().setSystemUiVisibility(uiOptions);
-        }
 
         copyAssets();
         InitCamera();
@@ -82,6 +77,20 @@ public class FaceSmashActivity extends SDLActivity {
     public void onPause() {
         super.onPause();
         ReleaseCamera();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     private void InitCamera() {
